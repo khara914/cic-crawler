947fc3e80cd22469b49b488314749a5105495eed
tm078 <takashi.maruyama.078@gmail.com>
2015-04-08 09:22:37 +0000

    add the attach category function

--- rss-put-dynamodb.py
+++ rss-put-dynamodb.py
@@ -9,6 +9,20 @@ from boto.dynamodb2.fields import HashKey, RangeKey
 from boto.dynamodb2.types import NUMBER
 from boto.dynamodb2.items import Item
 
+def attach_category(org_title):
+	chk_title = org_title.lower()
+	if 'cloud' in chk_title:
+		return 'cloud'
+	if 'aws' in chk_title:
+		return 'aws'
+	if 'iot' in chk_title:
+		return 'iot'
+	if 'bigdata' in chk_title:
+		return 'bigdata'
+	else:
+		return 'none'
+
+
 rssurl="http://54.64.75.5/?feed=rss2"
 #rssurl="http://rss.rssad.jp/rss/itmnews/2.0/news_society.xml"
 #rssurl="http://rss.rssad.jp/rss/itmnews/2.0/news_nettopics.xml"
@@ -22,12 +36,12 @@ print conn.list_tables()
 table_name = 'cic-tech-info'
 
 for i in range(5):
+	category = attach_category(fd.entries[i].title)
 	table = Table(table_name, connection=conn)
 	table.put_item(data={
-		'Category' : 'IoT',
+		'Category' : category,
 		'LastUpdateTime': fd.entries[i].published,
 		'URL': fd.entries[i].link,
 		'Title': fd.entries[i].title,
 		'Summary': fd.entries[i].summary,
-		'Tag1': fd.entries[i].tags,
-	})
+	},overwrite = True)
