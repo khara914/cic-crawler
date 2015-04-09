9d2bf6810d6b6b751958f95321de2b384f272729
tm078 <takashi.maruyama.078@gmail.com>
2015-04-09 09:36:45 +0000

    Add Img-URL search function

--- rss-put-dynamodb.py
+++ rss-put-dynamodb.py
@@ -3,11 +3,14 @@
 
 import boto.dynamodb2
 import feedparser
+import urllib2
 from boto.dynamodb2.layer1 import DynamoDBConnection
 from boto.dynamodb2.table import Table
 from boto.dynamodb2.fields import HashKey, RangeKey
 from boto.dynamodb2.types import NUMBER
 from boto.dynamodb2.items import Item
+from BeautifulSoup import BeautifulSoup
+
 
 def attach_category(org_title):
 	chk_title = org_title.lower()
@@ -22,8 +25,21 @@ def attach_category(org_title):
 	else:
 		return 'none'
 
+def get_img_url(site_url):
+        res = urllib2.urlopen(site_url)
+        soup = BeautifulSoup(res.read())
+        for link in soup.findAll('img'):
+                if 'alt' in str(link):
+                        if( link.has_key('src')):
+                                imgSrc = link['src']
+                                imgFile = imgSrc.split('/')[-1]
+                                ext = imgFile.split('.')[-1]
+                                if( ext == 'jpg' or ext == 'jpeg' or ext == 'png'):
+                                        img_url = link.get('src')
+                                        return img_url
+                                        break
 
-#rssurl="http://54.64.75.5/?feed=rss2"
+rssurl="http://54.64.75.5/?feed=rss2"
 #rssurl="http://rss.rssad.jp/rss/itmnews/2.0/news_society.xml"
 #rssurl="http://rss.rssad.jp/rss/itmnews/2.0/news_nettopics.xml"
 #rssurl="http://feeds.feedburner.com/AmazonWebServicesBlogJp?format=xml"
@@ -35,7 +51,8 @@ print conn.list_tables()
 
 table_name = 'cic-tech-info'
 
-for i in range(5):
+for i in range(3):
+	img_url = get_img_url(fd.entries[i].link)
 	category = attach_category(fd.entries[i].title)
 	table = Table(table_name, connection=conn)
 	table.put_item(data={
@@ -44,4 +61,6 @@ for i in range(5):
 		'URL': fd.entries[i].link,
 		'Title': fd.entries[i].title,
 		'Summary': fd.entries[i].summary,
+		'img_url': img_url,
+		'img_flag': 0,
 	},overwrite = True)
